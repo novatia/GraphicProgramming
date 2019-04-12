5191b5260546251f559be48aae9e0444c01b834e
nov4 <andrea.novati@gmail.com>
2019-04-12 11:43:47 +0200

    added Textures demo files

--- directx11-test-project/directx11-test/main.cpp
+++ directx11-test-project/directx11-test/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include <demo/box_demo/box_demo_app.h>
-#include <demo/lights_demo/lights_demo_app.h>
+#include <demo/textures_demo/textures_demo_app.h>
 
 
 using namespace xtest::application;
@@ -24,7 +24,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	directxSettings.showFrameStats = true;
 	
 
-	typedef xtest::demo::LightsDemoApp TestApp;
+	typedef xtest::demo::TextureDemoApp TestApp;
 
 	TestApp app(hInstance, windowSettings, directxSettings);
 	app.Init();
